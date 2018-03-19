1c8af3a0888c31c8608195052812cd65a40d0732
Christian Schneider <schnaader@gmx.de>
2018-03-19 15:46:09 +0100

    make.bat: Add -march to 32 and 64 bit

--- make.bat
+++ make.bat
@@ -11,10 +11,13 @@ set GPP64=x86_64-w64-mingw32-g++
 set GCC=%GCC64%
 set GPP=%GPP64%
 
+set MPARAM=-march=x86-64 -m64
+
 :parse
 if "%1"=="" goto endparse
 if "%1"=="32" (
   set GPP=%GPP32%
+  set MPARAM=-march=pentiumpro
 )
 :endparse
 
@@ -22,7 +25,7 @@ set PARSERSCPP=parsers\ddsparser.cpp parsers\modparser.cpp parsers\textparser.cp
 set TRANSFORMSCPP=transforms\zlibtransform.cpp
 set MAINCPP=analyser.cpp block.cpp deduper.cpp filestream.cpp hybridstream.cpp storagemanager.cpp
 
-%GPP% -O2 -std=c++11 -fomit-frame-pointer -s -static -static-libgcc -static-libstdc++ -Wall %TRANSFORMSCPP% %PARSERSCPP% %MAINCPP% -lz fairytale.cpp -ofairytale.exe
+%GPP% %MPARAM% -O2 -std=c++11 -fomit-frame-pointer -s -static -static-libgcc -static-libstdc++ -Wall %TRANSFORMSCPP% %PARSERSCPP% %MAINCPP% -lz fairytale.cpp -ofairytale.exe
 
 if not %ERRORLEVEL% == 0 echo ERROR!!!
 if %ERRORLEVEL% == 0 echo.
