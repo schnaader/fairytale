351bc7f4440d6b931fc205bc257ebd096ee1c2bc
Andrew Epstein <a.epstein@elsevier.com>
2018-03-28 01:31:12 -0400

    Changed copyright

--- parsers/jsonparser.cpp
+++ parsers/jsonparser.cpp
@@ -1,7 +1,7 @@
 /*
   This file is part of the Fairytale project
 
-  Copyright (C) 2018 Márcio Pais
+  Copyright (C) 2018 Andrew Epstein
 
   This library is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
